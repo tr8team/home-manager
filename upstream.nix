2bc5890091453ed362a0b53c9d05fbe23edc1557
Nicolas Darmadja <nicolas@tr8.io>
2025-09-26 11:57:58 +0800

    Update upstream.nix

--- upstream.nix
+++ upstream.nix
@@ -1,4 +1,4 @@
 import (builtins.fetchGit {
   url = "ssh://git@github.com/tr8team/home-manager-admin";
-  rev = "929e6d3dc2cd172735b045e04f4d280d8de95493";
+  rev = "c495f93dd0aedf8f438963935d1d5606a8fd33e5";
 })
